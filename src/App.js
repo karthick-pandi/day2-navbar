a9fd3664b1459615535d3c850861082294bde8fd
Karthick <93534549+karthick-pandi@users.noreply.github.com>
2022-04-13 12:35:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div>LOGO</div>
+      <div className="link">
+        <a href="service.js"> service</a>
+        <a href="project.js">project</a>
+        <a href="about.js">apout</a>
+      </div>
+      <button>contact</button>
+    </div>
+  );
+}
+s;
